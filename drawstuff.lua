2d9142e6d28561aa5f0c099efa06dcc876d96f8f
xandercreates <theironminecart6@gmail.com>
2025-11-01 01:02:24 -0400

    now we get serious

--- drawstuff.lua
+++ drawstuff.lua
@@ -1,58 +1,25 @@
 
 --#region Variables
----@class Button
-local buttons = {
-    _allButtons = {},
-    button1 = {
-        type = "toggle",
-        value = 0
-    },
-    button2 = {
-        type = "toggle",
-        value = 0
-    },
-    button3 = {
-        type = "toggle",
-        value = 0
-    },
-    button4 = {
-        type = "toggle",
-        value = 0
-    },
-    button5 = {
-        type = "toggle",
-        value = 0
-    },
-    b6 = {
-        type = "slider",
-        value = 0
-    }
-}
-
-local location = {
-    "up",
-    "down",
-    "left",
-    "right",
-    "center"
-}
-
-local pages = {
-
-}
-
-local monitor = peripheral.find("monitor")
-term.redirect(monitor)
-
-local desktop = paintutils.loadImage("backgr.nfp")
+local pos = term.setCursorPos
+local cls = term.clear
+local tCol = term.setTextColor
+local bCol = term.setBackgroundColor
+local box = paintutils.drawFilledBox
+local line = paintutils.drawLine
+local init = false
+local mon = peripheral.find("monitor")
 --#endregion
 
 --#region Funcs
-local function drawDesk()
-    paintutils.drawImage(desktop, 1, 1)
-end
+
 --#endregion
 
 --#region Loop
-
+while init == false do
+    print("Initialized.")
+    if not init then
+        init = true
+        break
+    end
+end
 --#endregion
