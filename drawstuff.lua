fa3cfce6ee3c97c4583a41ed8435c94511b0b130
xandercreates <theironminecart6@gmail.com>
2025-11-01 01:20:46 -0400

    testone

--- drawstuff.lua
+++ drawstuff.lua
@@ -1,34 +1,34 @@
 
 --#region Variables
-local pos = term.setCursorPos
-local cls = term.clear
-local tCol = term.setTextColor
-local bCol = term.setBackgroundColor
-local box = paintutils.drawFilledBox
-local line = paintutils.drawLine
+local function pos(...) return term.setCursorPos(...) end
+local function cls(...) return term.clear() end
+local function tCol(...) return term.setTextColor(...) end
+local function bCol(...) return term.setBackgroundColor(...) end
+local function box(...) return paintutils.drawFilledBox(...) end
+local function line(...) return paintutils.drawLine(...) end
 local init = false
 local mon = peripheral.find("monitor")
 
-local size = {
-    x = select(1, term.getSize()),
-    [1] = select(1, term.getSize()),
-    y = select(2, term.getSize()),
-    [2] = select(2, term.getSize())
-}
+local x, y = term.getSize()
 --#endregion
 
 --#region Funcs
-
+local function drawMenu()
+    cls()
+    pos(1,1)
+    box(5,5,7,2, colors.blue)
+end
+function main()
+    drawMenu()
+end
 --#endregion
 
 --#region Loop
 while init == false do
-    term.redirect(mon)
-    cls()
-    print("Initialized.")
-    print(size)
+    drawMenu()
     if not init then
         init = true
+        os.exit()
         break
     end
 end
