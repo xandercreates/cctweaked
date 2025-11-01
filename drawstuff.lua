a988d21aa4842f4d1390c458c33281c76d581c3f
xandercreates <theironminecart6@gmail.com>
2025-11-01 00:22:46 -0400

    wip

---
+++ drawstuff.lua
@@ -0,0 +1,52 @@
+
+--#region Variables
+---@class Button
+local buttons = {
+    _allButtons = {},
+    button1 = {
+        type = "toggle",
+        value = 0
+    },
+    button2 = {
+        type = "toggle",
+        value = 0
+    },
+    button3 = {
+        type = "toggle",
+        value = 0
+    },
+    button4 = {
+        type = "toggle",
+        value = 0
+    },
+    button5 = {
+        type = "toggle",
+        value = 0
+    },
+    b6 = {
+        type = "slider",
+        value = 0
+    }
+}
+
+local location = {
+    "up",
+    "down",
+    "left",
+    "right",
+    "center"
+}
+
+
+local desktop = paintutils.loadImage("backgr.nfp")
+--#endregion
+
+--#region Funcs
+local function drawDesk()
+    paintutils.drawImage(desktop, 1, 1)
+end
+--#endregion
+
+--#region Loop
+
+--#endregion
